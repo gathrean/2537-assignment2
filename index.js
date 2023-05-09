9cabb4b35a8f9fa60600d81d041b654eda3702b8
Gathrean Dela Cruz <gathrean@icloud.com>
2023-05-08 22:38:57 -0700

    Everything works but /admin redirects to /login regardless

--- index.js
+++ index.js
@@ -6,16 +6,11 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
+const Joi = require("joi");
+const app = express();
+const expireTime = 3600000; // 1 hour
 
 const port = process.env.PORT || 8080;
-
-const app = express();
-
-const Joi = require("joi");
-
-const expireTime = 24 * 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
-
-
 
 // Declaring variables to store sensitive information, such as database credentials and session secrets:
 
@@ -33,6 +28,11 @@ const node_session_secret = process.env.NODE_SESSION_SECRET;
 // reference to the users collection
 const { database } = require('./databaseConnection');
 const userCollection = database.db(mongodb_database).collection('users');
+
+userCollection.createIndex({ username: 1 }, { unique: true });
+userCollection.createIndex({ email: 1 }, { unique: true });
+userCollection.createIndex({ user_type: 1 });
+
 
 
 // Configuring express to use url-encoded data in request bodies
@@ -64,10 +64,54 @@ app.use(session({
 }));
 
 
-// A home page links to signup and login, if not logged in; and links to members and signout, if logged in.
+// HOME
 app.get('/', (req, res) => {
   res.render("index");
 });
+
+// Middleware function to check if the user is authenticated and has admin access
+const isAdmin = (req, res, next) => {
+  if (req.session.user && req.session.user.user_type === "admin") {
+    // If the user is an admin, allow them to access the /admin page
+    next();
+  } else {
+    // If the user is not an admin, redirect them to the /login page
+    res.redirect('/login');
+  }
+};
+
+
+
+
+
+
+function isValidSession(req) {
+  if (req.session.authenticated) {
+    return true;
+  }
+  return false;
+}
+
+function sessionValidation(req, res, next) {
+  if (isValidSession(req) && isAdmin(req)) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+function adminAuthorization(req, res, next) {
+  if (!isAdmin(req)) {
+    res.status(403);
+    res.render("errorMessage", { error: "Not Authorized" });
+    return;
+  }
+  else {
+    next();
+  }
+}
+
+
 
 app.get('/nosql-injection', async (req, res) => {
 
@@ -85,7 +129,7 @@ app.get('/nosql-injection', async (req, res) => {
 
   // Validate the username using the Joi library to prevent NoSQL injection attacks
   const schema = Joi.string().max(20).required();
-  const validationResult = schema.validate(username);
+  const validationResult = schema.validate(req.body);
 
   /*
    If we didn't use Joi to validate and check for a valid URL parameter below
@@ -96,7 +140,7 @@ app.get('/nosql-injection', async (req, res) => {
    */
 
   // if the validation failed, log an error and sends a message to the client 
-  if (validationResult.error != null) {
+  if (validationResult.error) {
     console.log(validationResult.error);
     res.send("<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>");
     return;
@@ -112,71 +156,144 @@ app.get('/nosql-injection', async (req, res) => {
   res.send(`<h1>Hello ${username}</h1>`);
 });
 
-app.get('/about', (req, res) => {
-  var color = req.query.color;
-  res.render("about", { color: color });
+// Route for the admin dashboard
+app.get('/admin', sessionValidation, adminAuthorization, (req, res) => {
+  res.render("admin");
+});
+
+
+//PROMO AND DEMOTE
+const ObjectId = require('mongodb').ObjectId;
+
+//PROMOTE AND DEMOTE
+app.post('/promoteUser', adminAuthorization, async (req, res) => {
+
+  try {
+    const userId = req.body.user_id;
+    const updatedUser = await userCollection.findOneAndUpdate(
+      { _id: ObjectId(userId) },
+      { $set: { user_type: 'admin' } }
+    );
+    if (!updatedUser) {
+      res.render("errorMessage", { error: "User not found" });
+      return;
+    }
+    res.redirect('/admin');
+  } catch (error) {
+    console.log(error);
+    res.render("errorMessage", { error: "Something went wrong" });
+  }
+});
+
+app.post('/demoteUser', adminAuthorization, async (req, res) => {
+
+  try {
+    const userId = req.body.user_id;
+    const updatedUser = await userCollection.findOneAndUpdate(
+      { _id: ObjectId(userId) },
+      { $set: { user_type: 'user' } }
+    );
+    if (!updatedUser) {
+      res.render("errorMessage", { error: "User not found" });
+      return;
+    }
+    res.redirect('/admin');
+  } catch (error) {
+    console.log(error);
+    res.render("errorMessage", { error: "Something went wrong" });
+  }
 });
 
 
 
+// SIGN UP
 app.get('/signup', (req, res) => {
   res.render("signup");
 });
 
 
 
+// Called when the user clicks the submit button on the login form.
+app.post('/submitUser', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // Define a new user object with default user_type value
+  const newUser = {
+    username,
+    email,
+    password,
+    user_type: 'user' // Set default user_type value
+  };
+
+  // Use Joi to validate the new user object
+  const schema = Joi.object({
+    username: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    user_type: Joi.string().valid('admin', 'user').required()
+  });
+
+  try {
+    await schema.validateAsync(newUser);
+
+    // Hash the password before storing it in the database
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // Save the user to the database
+    const result = await userCollection.insertOne({
+      username,
+      email,
+      password: hashedPassword,
+      user_type: newUser.user_type // Use the default user_type value from newUser object
+    });
+
+    console.log(`User ${username} registered successfully!`);
+    res.redirect('/login');
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(`Validation error: ${error.details[0].message}`);
+  }
+});
+
+
+
+// LOGIN
 app.get('/login', (req, res) => {
   res.render("login");
 });
 
 
-// Called when the user clicks the submit button on the login form.
-app.post('/submitUser', async (req, res) => {
-  var username = req.body.username;
-  var email = req.body.email;
-  var password = req.body.password;
 
-  const schema = Joi.object(
-    {
-      username: Joi.string().required(),
-      email: Joi.string().required(),
-      password: Joi.string().required()
-    });
+// Login route
+app.post('/login', async (req, res) => {
+  // Retrieve the username and password from the request body
+  const { username, password } = req.body;
 
-  const validationResult = schema.validate({ username, email, password });
-  if (validationResult.error != null) {
-    console.log(validationResult.error);
-    res.redirect("/signup");
-    return;
-  }
+  // Retrieve the user from the database based on the username
+  const user = await userCollection.findOne({ username: username });
 
-  var hashedPassword = await bcrypt.hash(password, saltRounds);
-
-  try {
-    const existingUser = await userCollection.findOne({ email: email });
-    if (existingUser) {
-      console.log("Email already registered!");
-      res.redirect("/signup");
-    } else {
-      const newUser = {
-        username: username,
-        email: email,
-        password: hashedPassword,
-      };
-
-      const insertResult = await userCollection.insertOne(newUser);
-      console.log(insertResult);
-      res.redirect("/members"); // add this line
+  // Check if a user was found and if the password matches the hashed password in the database
+  if (user && await bcrypt.compare(password, user.password)) {
+    // If the user is an admin, set the isAdmin property in the session object to true
+    if (user.user_type === 'admin') {
+      req.session.isAdmin = true;
     }
-  } catch (error) {
-    console.log(error);
-    res.redirect("/signup");
+    // Set the authenticated property in the session object to true
+    req.session.authenticated = true;
+    // Set the username property in the session object to the logged-in user's username
+    req.session.username = username;
+    // Redirect the user to the home page
+    res.redirect('/');
+  } else {
+    // If the user was not found or the password did not match, display an error message
+    res.render('login', { error: 'Invalid username or password' });
   }
 });
 
 
 
-// Called when the user clicks the submit button on the login form.
+
+// LOGGING IN
 app.post('/loggingin', async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -214,30 +331,21 @@ app.get('/loggedin', (req, res) => {
 });
 
 
-// Array for random images found in public folder
-const images = ["image1.jpeg", "image2.jpeg", "image3.jpeg"];
-const getRandomImage = () => images[Math.floor(Math.random() * images.length)];
-
-
 // Members page that only logged in users can access
 app.get('/members', (req, res) => {
   const username = req.session.username;
-  const image = getRandomImage();
+  const image1 = "graduation.gif";
+  const image2 = "yandhi.gif";
+  const image3 = "kanye.gif";
+
   if (!username) {
     res.redirect('/login');
   } else {
-    res.send(`
-    <h1>Welcome back, ${username}!</h1>
-    
-    <form action="/logout" method="POST">
-      <button type="submit">Logout</button>
-    </form>
-
-    <p> Here's a random photo of an orca to improve your day!</p>
-    <img src="${image}" alt="Random Orca image for your day!">
-  `);
+    res.render('members', { username, image1, image2, image3 });
   }
 });
+
+
 
 // Serve static files from the 'public' folder
 app.use(express.static('public'));
